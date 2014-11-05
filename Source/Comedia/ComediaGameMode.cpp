2edd4d4897ea6814b72f8e27a7c686203d9e42c5
unknown <daniel.quach@STUD-VDOC-049.formation.enjmin.fr>
2014-11-05 14:24:24 +0100

    UE Project initial commit

---
+++ Source/Comedia/ComediaGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Comedia.h"
+#include "ComediaGameMode.h"
+
+
+AComediaGameMode::AComediaGameMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/Comedia/ComediaGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ComediaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMEDIA_API AComediaGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
