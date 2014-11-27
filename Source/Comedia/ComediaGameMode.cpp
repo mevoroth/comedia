fa62cfc464509e3f23eabaea72b5b6692e6f606c
Antoine TEUF <antoine.teuf@gmail.com>
2014-11-27 18:53:00 +0100

    Ajout camera fps et controle clavier

--- Source/Comedia/ComediaGameMode.cpp
+++ Source/Comedia/ComediaGameMode.cpp
@@ -7,6 +7,10 @@
 AComediaGameMode::AComediaGameMode(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
+	//Set default pawn class to our blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/FPSCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
 
 }
 
