37079591d06e1107cd7e758d1c40ce91aa98d6a3
Antoine TEUF <antoine.teuf@gmail.com>
2015-02-03 17:44:56 +0100

    # Continue Propaganda Hand

--- Source/Comedia/PropagandaHandActor.cpp
+++ Source/Comedia/PropagandaHandActor.cpp
@@ -3,3 +3,16 @@
 #include "Comedia.h"
 #include "PropagandaHandActor.h"
 
+APropagandaHandActor::APropagandaHandActor(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	//Add Arrow Component to see propaganda hand orientation
+	ForwardArrowComponent = ObjectInitializer.CreateDefaultSubobject<UArrowComponent>(this, TEXT("ForwardArrowComponent"));
+	RootComponent = ForwardArrowComponent;
+
+	//Add SphereTriggerZoneComponent to detect player presence
+	SphereTriggerZoneComponent = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, TEXT("SphereTriggerZoneComponent"));
+	SphereTriggerZoneComponent->AttachTo(RootComponent);
+
+	PrimaryActorTick.bCanEverTick = true;
+}
